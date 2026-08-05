1a708f576e9ca335417b428115d4cae1b8a12751
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-08-05 21:45:35 +1100

    Add files via upload

--- meta-platform/virtual_processor/driv3r.c
+++ meta-platform/virtual_processor/driv3r.c
@@ -1,1 +1,14 @@
-void driv3r(){}
+void driv3r()
+{
+    char opcodes[] =
+    {
+        0
+    };
+    void * dispatch[0x100] =
+    {
+        [0 ... 0xFF] = &&__0
+    };
+    goto * dispatch[ opcodes[0] ];
+    __0:
+     { return; }
+}
