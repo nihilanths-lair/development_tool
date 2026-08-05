7e58591c9dc5d942b4e05d6922ec5a3c3d2c30da
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-08-05 22:14:14 +1100

    Add files via upload

--- meta-platform/virtual_processor/driv3r.c
+++ meta-platform/virtual_processor/driv3r.c
@@ -1,17 +1,25 @@
 void driv3r()
 {
-    char memory[] =
+    unsigned char memory[] =
     {
         0
     };
     void * opcodes[ 0x100 ] =
     {
-        [0 ... 0xFF] = &&__1
+        [0         ] = &&__1,
+        [1 ... 0xFF] = &&__2
     };
-    goto * opcodes[* memory];
+    printf(" [<<{ TRACING }>>] logical_opcode_%u | %s ; %s", * memory, "-", "-");
+    goto * (opcodes + * memory);
     __1:
     {
         printf(" logical_opcode_%u | %s ; %s", 1, "HLT", "Прекратить выполнение каких-либо инструкций");
         return;
     }
+    __2:
+    {
+        printf(" logical_opcode_%u | %s ; %s", 2, "-", "(Неизвестный / Несуществующий) опкод");
+        return;
+    }
+    //printf(" logical_opcode_%u | %s ; %s", * memory, "-", "-");
 }
