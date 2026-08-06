f89ca77994e4b5b58712b41afcf6de511fe817e7
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-08-06 22:37:06 +1100

    Add files via upload

--- meta-platform/virtual_processor/driv3r.c
+++ meta-platform/virtual_processor/driv3r.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
+#include <stdint.h>
 
 void driv3r()
 {
-    unsigned char memory[0x100] = {0};
+    // гибридная модель памяти
+    uint8_t m8[0x100] = {0}; // для хранения строк
+    uint64_t m64[0x100] = {0}; // для хранения чисел
+    //uint64_t pc;
+
     FILE * fp = fopen("virtual_processor/meta_language", "r");
     if (!fp)
     {
         printf("\n /!\\: Не удалось открыть файл ...");
         return;
     }
+
     fseek(fp, 0, SEEK_END);
     long file_size = ftell(fp);
     fseek(fp, 0, SEEK_SET);
-    for (unsigned short i = 0; i < file_size; i++) printf("\n [%+2u] = %02X", i+1, memory[i]);
+
+    for (unsigned short i = 0; i < file_size; i++) printf("\n [%+2u] = %02X", i+1, m8[i]);
     putchar('\n');
     unsigned char ascii = 0;
-    for (unsigned char i = 0; fscanf(fp, "%hhx", &memory[i]) != EOF; i++, ascii++) {}
+    for (unsigned char i = 0; fscanf(fp, "%hhx", &m8[i]) != EOF; i++, ascii++) {}
     fclose(fp);
-    for (unsigned short i = 0; i < ascii; i++) printf("\n [%+2u] = %02X", i+1, memory[i]);
+    for (unsigned short i = 0; i < ascii; i++) printf("\n [%+2u] = %02X", i+1, m8[i]);
     putchar('\n');
     unsigned char program_counter = 0;
     void * opcodes[ 0x100 ] =
@@ -27,8 +34,8 @@ void driv3r()
         [2 ... 0xFF] = &&__3
     };
     //unsigned char number_of_clock_cycles = 0; // Количество тактов
-    //printf(" {{ TRACING }} logical_opcode_%u | %s ; %s", * memory, "-", "-");
-    goto ** (opcodes + * (memory + program_counter));
+    //printf(" {{ TRACING }} logical_opcode_%u | %s ; %s", * m8, "-", "-");
+    goto ** (opcodes + * (m8 + program_counter));
     __1:
     {
         //number_of_clock_cycles++;
@@ -40,7 +47,7 @@ void driv3r()
         //number_of_clock_cycles++;
         printf("\n logical_opcode_%u | %s ; %s", 2, "NOP / SKIP / IGNORE / STEP_FORWARD", "Пропустить / Игнорировать / Сделать шаг вперёд");
         program_counter++;
-        goto ** (opcodes + * (memory + program_counter));
+        goto ** (opcodes + * (m8 + program_counter));
     }
     __3:
     {
